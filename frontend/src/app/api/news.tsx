3cb93a3cebb8b9771e70f062f2cd3e8997bb1e07
Felix Mulya <felixmulya777@gmail.com>
2024-12-26 00:35:01 +0700

    fix: naming api

--- frontend/src/app/api/news.tsx
+++ frontend/src/app/api/news.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_KEY;
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export const fetchNews = async (category: string = 'saham') => {
   try {
--- frontend/src/app/api/stock.tsx
+++ frontend/src/app/api/stock.tsx
@@ -1,6 +1,6 @@
 import axios, { AxiosError } from 'axios';
 
-const API_URL = process.env.NEXT_PUBLIC_API_KEY;
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 interface Fundamentals {
     marketCap: number | null;
